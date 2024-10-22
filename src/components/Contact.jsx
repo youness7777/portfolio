84e0d0e43b67c968d210ca6ed84d1afe1c4a8979
youness7777 <57494358+youness7777@users.noreply.github.com>
2024-10-22 18:37:50 +0100

    Contact section

---
+++ src/components/Contact.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+
+export const Contact = () => {
+  return (
+    <div className='h-auto w-full bg-gradient-to-b from-black p-4 to-gray-800 '>
+        <div className='flex flex-col justify-center p-4 max-w-screen-lg text-white mx-auto '>
+            <div className='pb-8'>
+                <p className='text-4xl font-bold inline border-b-4 border-gray-500'>
+                    Contact
+                </p>
+                    
+                <p className='py-6'>
+                    vous pouvez envoyer votre interet depuis la formulaire en bas
+                </p>
+            </div>
+
+            <div className='flex flex-col justify-center items-center'>
+                <form action='' className='flex flex-col w-full md:w-1/2'>
+                        <input className=' my-4 p-2 bg-transparent border-2 rounded-md text-white focus:outline-none' type='text' placeholder='Entrer votre nom :' required></input>
+                        <input className=' my-4 p-2 bg-transparent border-2 rounded-md text-white focus:outline-none' type='text' placeholder='Entrer votre Email :' required></input>
+                        <textarea rows={10} className=' my-4 p-2 bg-transparent border-2 rounded-md text-white focus:outline-none' type='textbox' placeholder='Entrer votre Message :' required />
+                        <button className='mx-auto text-white bg-gradient-to-b from-cyan-500 to-blue-500 px-6 py-3 rounded-md hover:scale-110 duration-300'     > Envoyer</button>
+
+
+                </form>
+
+            </div>
+        </div>
+    </div>
+  )
+}
